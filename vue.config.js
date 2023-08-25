0c18ce047a219b102ac5f11db3666f5599add076
dbl <3245791066@qq.com>
2023-08-25 15:54:17 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/vue3_study',//github仓库名
-  assetsDir: 'vue3_study',//同上，否则访问不到静态资源
-  outputDir: "dist",
+  // publicPath: '/vue3_study',//github仓库名
+  // assetsDir: 'vue3_study',//同上，否则访问不到静态资源
+  // outputDir: "dist",
   transpileDependencies: true,
   lintOnSave: false,
   css: {
