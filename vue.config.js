dd1e50550886fa0f754d67207aa4d059ac94a61c
dbl <3245791066@qq.com>
2023-08-25 17:55:07 +0800

    刚才是失误, 代码run build 报错了😀

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base: '/vueBuild/',
+  publicPath: '/vueBuild/',
   transpileDependencies: true,
   lintOnSave: false,
   css: {
